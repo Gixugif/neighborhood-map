7a1cbdea12af00ab5d58277248d2f6a8f8d3848a
Jeffrey <zic.jeffrey@gmail.com>
2016-04-10 17:00:21 -0400

    feat: simplify creation of map

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,10 @@
-ko.bindingHandlers.map = {
+var self = this;
 
-	init: function(element,valueAccessor,allValuesAccessor,viewModel) {
-		var mapObj = ko.utils.unwrapObservable(valueAccessor());
-		var latLng = new google.map.LatLng(
-			ko.utils.unwrapObservable(mapObj.lat),
-			ko.utils.unwrapObservable(mapObj.lng));
-		var mapOptions = { center: latLng,
-							zoom: 10,
-							mapTypeId: google.maps.mapTypeId.ROADMAP};
+var map;
 
-		mapObj.googleMap = new google.maps.Map(element, mapOptions);
-
-	}
-};
-
-/*function MapViewModel() {
-	var self = this;
-
-	self.lat = data.Lat;
-	self.lng = data.lng;
-
-	var map;
-
-	function initMap() {
-	  map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: 41.994654, lng: -73.875959},
-	    zoom: 15
-	  });
-	}
-}*/
-
-ko.applyBindings(new mapViewModel());
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 41.994654, lng: -73.875959},
+		zoom: 15
+	});
+}
