ce4c9b7e97c6043f3339a985df10799e55c11759
Jeffrey <zic.jeffrey@gmail.com>
2016-04-10 14:17:56 -0400

    feat: rewrite bindingHandler for map

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,33 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.994654, lng: -73.875959},
-    zoom: 15
-  });
-}
+ko.bindingHandlers.map = {
+
+	init: function(element,valueAccessor,allValuesAccessor,viewModel) {
+		var mapObj = ko.utils.unwrapObservable(valueAccessor());
+		var latLng = new google.map.LatLng(
+			ko.utils.unwrapObservable(mapObj.lat),
+			ko.utils.unwrapObservable(mapObj.lng));
+		var mapOptions = { center: latLng,
+							zoom: 10,
+							mapTypeId: google.maps.mapTypeId.ROADMAP};
+
+		mapObj.googleMap = new google.maps.Map(element, mapOptions);
+
+	}
+};
+
+/*function MapViewModel() {
+	var self = this;
+
+	self.lat = data.Lat;
+	self.lng = data.lng;
+
+	var map;
+
+	function initMap() {
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: 41.994654, lng: -73.875959},
+	    zoom: 15
+	  });
+	}
+}*/
+
+ko.applyBindings(new mapViewModel());
