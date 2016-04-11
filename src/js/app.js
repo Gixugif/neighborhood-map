6878deb93206bbf0fbbf328aeb25caa517de65ae
Jeffrey <zic.jeffrey@gmail.com>
2016-04-11 12:10:55 -0400

    fix: move marker listener to init
    Moved marker event listener to init function so it is not referenced before
    the maps api is loaded.

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,30 @@
 var map;
 
 function initMap() {
-	var latLng = {lat: 41.994654, lng: -73.875959}
+	var latLng = {lat: 41.994654, lng: -73.875959};
+	var markerArray = [];
 
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: latLng,
 		zoom: 15
 	});
 
+	google.maps.event.addDomListener(window,'load',addMarker({lat: 41.994654, lng: -73.875959},map,'Red Hook, NY'));
+
+}
+
+function addMarker(latLng,map,name) {
+
 	var marker = new google.maps.Marker({
 		position: latLng,
 		map: map,
-		title: 'Red Hook, NY'
-	})
+		animation: google.maps.Animation.DROP,
+		title: name
+	});
 }
+
+
+
+
 
 
