8dbf893d937c0ca943ecfeb1b9c098b1053645d9
Jeffrey <zic.jeffrey@gmail.com>
2016-04-11 10:43:56 -0400

    feat: add map marker to center location

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 var map;
 
 function initMap() {
+	var latLng = {lat: 41.994654, lng: -73.875959}
+
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 41.994654, lng: -73.875959},
+		center: latLng,
 		zoom: 15
 	});
+
+	var marker = new google.maps.Marker({
+		position: latLng,
+		map: map,
+		title: 'Red Hook, NY'
+	})
 }
+
+
