946b3f9e3d3f109f6405860b7343b0bf062a0799
Jeffrey <zic.jeffrey@gmail.com>
2016-04-27 21:00:56 -0400

    feat: add values for Yelp oauth authentication

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,24 @@
 var map;
 
+
+var randomString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+/*Yelp API oauth values*/
+var oauth_consumer_key = 'o7io1ZSwVn_Gcxj4MqsgkQ';
+var oauth_token = '3Rka9DQ66lV8bxGac4DSEcgt6Ze95TfM';
+var oauth_signature_method = 'hmac-sha1';
+var oauth_signature = 'oZstoMIkks8R3kXJGM5uFzDipas';
+var oauth_timestamp = (new Date).getTime() * 1000;
+var oauth_nonce = randomString(10);
+
+
 function initMap() {
 	var latLng = {lat: 41.994654, lng: -73.875959};
 	var markerArray = [];
@@ -34,8 +53,3 @@ filterBox.focus(function() {
 close.click(function() {
 	menu.removeClass('is-active');
 });
-
-
-
-
-
