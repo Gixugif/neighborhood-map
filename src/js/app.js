59dc106cfd59632e445aaf94f9946b2f0a37b952
Jeffrey <zic.jeffrey@gmail.com>
2016-04-30 16:03:11 -0400

    fix: fix ajax request
    Ajax request was constructed impropery. Constructed it properly using a third
    party oauth signature generator

--- src/js/app.js
+++ src/js/app.js
@@ -11,13 +11,10 @@ var randomString = function(length) {
 }
 
 /*Yelp API oauth values*/
-var oauth_consumer_key = '&oauth_consumer_key=';
+
 var oauth_consumer_keyVal = 'o7io1ZSwVn_Gcxj4MqsgkQ';
-var oauth_token = '&oauth_token=';
 var oauth_tokenVal = '3Rka9DQ66lV8bxGac4DSEcgt6Ze95TfM';
-var oauth_signature_method = '&oauth_signature_method=';
-var oauth_signature_methodVal = 'hmac-sha1';
-var oauth_signature = '&oauth_signature='
+var oauth_signature_methodVal = 'HMAC-SHA1';
 var oauth_signatureVal = 'oZstoMIkks8R3kXJGM5uFzDipas';
 var oauth_timestamp = '&oauth_timestamp=';
 var oauth_nonce = '&oauth_nonce=';
@@ -25,7 +22,7 @@ var oauth_nonce = '&oauth_nonce=';
 /*Yelp search URL values*/
 var baseURL = 'https://api.yelp.com/v2/search?';
 var term = 'term=';
-var location = '&location=';
+var location = '\&location=';
 var category = '&category_filter=';
 
 
@@ -52,22 +49,52 @@ function addMarker(latLng,map,name) {
 	});
 }
 
-function searhYelp(termVal,locationVal,categoryVal) {
-	var searchURL = baseURL + term + termVal + location + locationVal + category + categoryVal + \
-	oauth_consumer_key + oauth_consumer_keyVal + oauth_token + oauth_tokenVal + oauth_signature_method + oauth_signature_methodVal + \
-    oauth_signature + oauth_signatureVal + oauth_timestamp + (new Date).getTime() * 1000 + oauth_nonce + randomString(10);
-
-    var response = function reqListener () {
-         return this.responseText;
-        }
-
-        var oReq = new XMLHttpRequest();
-        oReq.addEventListener('load',reqListener);
-        oReq.open('GET',searchURL);
-        oReq.send();
-
-    return response;
+function nonce_generate() {
+  return (Math.floor(Math.random() * 1e12).toString());
 }
+
+function searchYelp(termVal,locationVal,categoryVal) {
+	//var searchURL = baseURL + term + termVal + '\&' + 'location=' + locationVal + category + categoryVal
+	//console.log(searchURL);
+
+	var httpMethod = 'GET',
+		url = 'http://api.yelp.com/v2/search?',
+		parameters = {
+			location: locationVal,
+			term: termVal,
+			category_filter: categoryVal,
+			oauth_consumer_key: oauth_consumer_keyVal,
+			oauth_token: oauth_tokenVal,
+			oauth_nonce: nonce_generate(),
+			oauth_timestamp: Math.floor(Date.now()/1000),
+			oauth_signature_method: oauth_signature_methodVal,
+			callback: 'cb'
+		},
+		consumerSecret = 'Xzz7g7qJMMY6-rs8zX8KTtxtBZY',
+		tokenSecret = 'oZstoMIkks8R3kXJGM5uFzDipas',
+		encodedSignature = oauthSignature.generate(httpMethod,url,parameters,consumerSecret,tokenSecret);
+		parameters.oauth_signature = encodedSignature;
+
+		console.log(encodedSignature);
+
+		var settings = {
+			url: url,
+			data: parameters,
+			cache: true,
+			dataType: 'jsonp',
+			jsonpCallback: 'cb',
+			success: function(results) {
+				console.log(results);
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		};
+
+		$.ajax(settings);
+}
+
+searchYelp('food','Red+Hook,NY+12571','diners');
 
 var filterBox = $('.filter-box');
 var menu = $('.filter-menu');
