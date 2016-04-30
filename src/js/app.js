28079c26214f76bb9b7ac43360a465a7aaedd2b2
Jeffrey <zic.jeffrey@gmail.com>
2016-04-30 13:12:27 -0400

    feat: add function to make ajax request to Yelp search
    Added function to make ajax requests to the Yelp search API

--- src/js/app.js
+++ src/js/app.js
@@ -11,12 +11,22 @@ var randomString = function(length) {
 }
 
 /*Yelp API oauth values*/
-var oauth_consumer_key = 'o7io1ZSwVn_Gcxj4MqsgkQ';
-var oauth_token = '3Rka9DQ66lV8bxGac4DSEcgt6Ze95TfM';
-var oauth_signature_method = 'hmac-sha1';
-var oauth_signature = 'oZstoMIkks8R3kXJGM5uFzDipas';
-var oauth_timestamp = (new Date).getTime() * 1000;
-var oauth_nonce = randomString(10);
+var oauth_consumer_key = '&oauth_consumer_key=';
+var oauth_consumer_keyVal = 'o7io1ZSwVn_Gcxj4MqsgkQ';
+var oauth_token = '&oauth_token=';
+var oauth_tokenVal = '3Rka9DQ66lV8bxGac4DSEcgt6Ze95TfM';
+var oauth_signature_method = '&oauth_signature_method=';
+var oauth_signature_methodVal = 'hmac-sha1';
+var oauth_signature = '&oauth_signature='
+var oauth_signatureVal = 'oZstoMIkks8R3kXJGM5uFzDipas';
+var oauth_timestamp = '&oauth_timestamp=';
+var oauth_nonce = '&oauth_nonce=';
+
+/*Yelp search URL values*/
+var baseURL = 'https://api.yelp.com/v2/search?';
+var term = 'term=';
+var location = '&location=';
+var category = '&category_filter=';
 
 
 function initMap() {
@@ -40,6 +50,12 @@ function addMarker(latLng,map,name) {
 		animation: google.maps.Animation.DROP,
 		title: name
 	});
+}
+
+function searhYelp(termVal,locationVal,categoryVal) {
+	var searchURL = baseURL + term + termVal + location + locationVal + category + categoryVal + \
+	oauth_consumer_key + oauth_consumer_keyVal + oauth_token + oauth_tokenVal + oauth_signature_method + oauth_signature_methodVal + \
+    oauth_signature + oauth_signatureVal + oauth_timestamp + (new Date).getTime() * 1000 + oauth_nonce + randomString(10);
 }
 
 var filterBox = $('.filter-box');
